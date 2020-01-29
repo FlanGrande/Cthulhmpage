2065505be84203db75c75c11b56289928013a86c
Flan <francisjgrande@gmail.com>
2020-01-29 22:33:33 +0000

    Physics added
    First time flirting with physics.

---
+++ scripts/PhysicsBlock.gd
@@ -0,0 +1,149 @@
+extends RigidBody2D
+
+export var scale_factor = Vector2(1, 1)
+
+# If the block is rotated in a direction, this part of the block is facing upwards.
+# 0 degrees, upwards side is TOP.
+# 90 degrees, upwards side is the LEFT.
+# 180 degrees, upwards side is BOTTOM.
+# 270 degrees, upwards side is the RIGHT side.
+enum UPWARDS_DIRECTION {
+	TOP,
+	RIGHT,
+	BOTTOM,
+	LEFT
+}
+
+var physics_enabled = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	switch_climbing_borders(false)
+	switch_collision_borders(UPWARDS_DIRECTION.TOP)
+	
+	print("//////////////////////START")
+	
+	print("Sprite position:\t\t", $Sprite.position)
+	print("Sprite scale:\t\t", $Sprite.scale)
+	print("CollisionShape2D Position:\t\t", $CollisionShape2D.position)
+	print("CollisionShape2D scale:\t\t", $CollisionShape2D.scale)
+	print("ClimbingTop Position:\t\t", $ClimbingAreas/Top.position)
+	print("ClimbingTop scale:\t\t", $ClimbingAreas/Top.scale)
+	print("CollTop Position:\t\t", $Colliders/CollTop.position)
+	print("CollTop scale:\t\t", $Colliders/CollTop.scale)
+	print("//////////////////////")
+	
+	fit_to_scale(scale_factor)
+	
+	print("//////////////////////")
+	print("Sprite position:\t\t", $Sprite.position)
+	print("Sprite scale:\t\t", $Sprite.scale)
+	print("CollisionShape2D Position:\t\t", $CollisionShape2D.position)
+	print("CollisionShape2D scale:\t\t", $CollisionShape2D.scale)
+	print("ClimbingTop Position:\t\t", $ClimbingAreas/Top.position)
+	print("ClimbingTop scale:\t\t", $ClimbingAreas/Top.scale)
+	print("CollTop Position:\t\t", $Colliders/CollTop.position)
+	print("CollTop scale:\t\t", $Colliders/CollTop.scale)
+
+	print("//////////////////////END")
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+# warning-ignore:unused_argument
+func _physics_process(delta):
+	if(physics_enabled):
+		var rot = abs(int(rotation_degrees))
+		var sin_x = stepify(sin(rotation), 0.1)
+		var cos_x = stepify(cos(rotation), 0.1)
+		
+		var orientation = Vector2(cos_x, sin_x)
+		
+		print(orientation)
+		
+		match orientation:
+			Vector2(1.0, 0.0):
+				switch_climbing_borders(false)
+				switch_collision_borders(UPWARDS_DIRECTION.TOP)
+			Vector2(0.0, 1.0):
+				switch_climbing_borders(true)
+				switch_collision_borders(UPWARDS_DIRECTION.LEFT)
+			Vector2(-1.0, 0.0):
+				switch_climbing_borders(false)
+				switch_collision_borders(UPWARDS_DIRECTION.BOTTOM)
+			Vector2(0.0, -1.0):
+				switch_climbing_borders(true)
+				switch_collision_borders(UPWARDS_DIRECTION.RIGHT)
+	else:
+		
+		pass
+
+func switch_climbing_borders(invert):
+	var climbing_areas = $ClimbingAreas.get_children()
+	
+	climbing_areas[0].disabled = not invert #Top
+	climbing_areas[1].disabled = invert #Right
+	climbing_areas[2].disabled = not invert #Bottom
+	climbing_areas[3].disabled = invert #Left
+
+func switch_collision_borders(upwards_side):
+	var colliders = $Colliders.get_children()
+	
+	match upwards_side:
+		UPWARDS_DIRECTION.TOP:
+			colliders[0].get_node("CollisionShape2D").disabled = false #Top
+			colliders[1].get_node("CollisionShape2D").disabled = true #Right
+			colliders[2].get_node("CollisionShape2D").disabled = true #Bottom
+			colliders[3].get_node("CollisionShape2D").disabled = true #Left
+		UPWARDS_DIRECTION.RIGHT:
+			colliders[0].get_node("CollisionShape2D").disabled = true #Top
+			colliders[1].get_node("CollisionShape2D").disabled = false #Right
+			colliders[2].get_node("CollisionShape2D").disabled = true #Bottom
+			colliders[3].get_node("CollisionShape2D").disabled = true #Left
+		UPWARDS_DIRECTION.BOTTOM:
+			colliders[0].get_node("CollisionShape2D").disabled = true #Top
+			colliders[1].get_node("CollisionShape2D").disabled = true #Right
+			colliders[2].get_node("CollisionShape2D").disabled = false #Bottom
+			colliders[3].get_node("CollisionShape2D").disabled = true #Left
+		UPWARDS_DIRECTION.LEFT:
+			colliders[0].get_node("CollisionShape2D").disabled = true #Top
+			colliders[1].get_node("CollisionShape2D").disabled = true #Right
+			colliders[2].get_node("CollisionShape2D").disabled = true #Bottom
+			colliders[3].get_node("CollisionShape2D").disabled = false #Left
+
+func fit_to_scale(factor):
+	# Sprite
+	$Sprite.scale *= factor
+	
+	# Collision Shape
+	$CollisionShape2D.scale *= factor
+	
+	# Climbing Areas
+	$ClimbingAreas/Top.position *= factor
+	$ClimbingAreas/Top.scale *= factor
+	$ClimbingAreas/Right.position *= factor
+	$ClimbingAreas/Right.scale *= factor
+	$ClimbingAreas/Bottom.position *= factor
+	$ClimbingAreas/Bottom.scale *= factor
+	$ClimbingAreas/Left.position *= factor
+	$ClimbingAreas/Left.scale *= factor
+	
+	# Colliders
+	$Colliders/CollTop.position *= factor
+	$Colliders/CollTop.scale *= factor
+	$Colliders/CollRight.position *= factor
+	$Colliders/CollRight.scale *= Vector2(factor.y, factor.x) # This collider is rotated 90 degrees
+	$Colliders/CollBottom.position *= factor
+	$Colliders/CollBottom.scale *= factor
+	$Colliders/CollLeft.position *= factor
+	$Colliders/CollLeft.scale *= Vector2(factor.y, factor.x) # This collider is rotated 90 degrees
+
+func _on_ClimbingAreas_body_entered(body):
+	if(body.is_in_group("punch")):
+		call_deferred("punch_received")
+	
+	if(body.is_in_group("block") and body.mode == RigidBody2D.MODE_RIGID):
+		call_deferred("punch_received")
+
+func punch_received():
+	mode = RigidBody2D.MODE_RIGID
--- scripts/characters/Cthulhu.gd
+++ scripts/characters/Cthulhu.gd
@@ -5,7 +5,8 @@ enum State {
 	STATE_CLIMB,
 	STATE_FALL,
 	STATE_JUMP,
-	STATE_GLIDE
+	STATE_GLIDE,
+	STATE_ATTACK
 }
 
 var current_state = State.STATE_WALK
@@ -19,13 +20,19 @@ var climb_speed = 120.0
 var jump_speed = Vector2(100.0, -120.0)
 var glide_speed = Vector2(320.0, 40.0)
 
+# Array containing the nearest buildings, sorted by distance (first item is the closest)
+var nearest_buildings_array = []
+
 var current_building_to_climb_anchor_node : Node
 var can_climb = false
 var falling = false
+var attacking = false
+var state_after_attack = State.STATE_WALK
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	current_state = State.STATE_FALL
+	change_animation("idle")
 
 func _process(delta):
 	handle_input()
@@ -33,16 +40,44 @@ func _process(delta):
 	pass
 
 func update():
-	print("STATE: " + str(current_state))
+	#print("CURRENT STATE: " + str(current_state))
 	
-	if(current_state != State.STATE_CLIMB and current_state != State.STATE_JUMP and current_state != State.STATE_GLIDE):
-		if(not is_on_floor()):
-			current_state = State.STATE_FALL
-		else:
-			current_state = State.STATE_WALK
+	if(nearest_buildings_array.size() > 0):
+		can_climb = true
+	else:
+		can_climb = false
 	
-	if(current_state == State.STATE_CLIMB and not can_climb):
-		current_state = State.STATE_FALL
+	if(not $AnimationPlayer.is_playing()):
+		current_state = state_after_attack
+		attacking = false
+		change_animation("idle")
+	
+	match current_state:
+		State.STATE_WALK:
+			if(not is_on_floor()):
+				current_state = State.STATE_FALL
+			else:
+				current_state = State.STATE_WALK
+		
+		State.STATE_CLIMB:
+			if(not can_climb and not attacking):
+				current_state = State.STATE_FALL
+			pass
+		
+		State.STATE_FALL:
+			if(not is_on_floor()):
+				current_state = State.STATE_FALL
+			else:
+				current_state = State.STATE_WALK
+		
+		State.STATE_JUMP:
+			pass
+		
+		State.STATE_GLIDE:
+			pass
+		
+		State.STATE_ATTACK:
+			pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -51,6 +86,10 @@ func _physics_process(delta):
 func _input(event):
 	if(Input.is_action_pressed("ui_exit")):
 		get_tree().quit()
+
+func change_animation(new_animation_name):
+	if($AnimationPlayer.current_animation != new_animation_name):
+		$AnimationPlayer.play(new_animation_name)
 
 func handle_input():
 	#print(str(current_state))
@@ -63,14 +102,17 @@ func handle_input():
 				speed.x = walk_speed
 			else:
 				speed.x = 0
-				
+			
 			if(can_climb and (Input.is_action_pressed("ui_up"))):
 				current_state = State.STATE_CLIMB
 				hug_wall()
 			
-			if(not can_climb and is_on_floor() and Input.is_action_pressed("ui_up")):
+			if(is_on_floor() and Input.is_action_pressed("jump")):
 				speed = Vector2(sign(speed.x) * jump_speed.x, jump_speed.y)
 				current_state = State.STATE_JUMP
+			
+			if(Input.is_action_pressed("attack")):
+				attack(State.STATE_WALK)
 		
 		State.STATE_CLIMB:
 			speed.x = 0
@@ -84,6 +126,9 @@ func handle_input():
 					current_state = State.STATE_WALK
 			else:
 				speed.y = 0
+			
+			if(Input.is_action_pressed("attack")):
+				attack(State.STATE_CLIMB)
 		
 		State.STATE_FALL:
 			speed = Vector2(0, gravity * speed_factor)
@@ -99,8 +144,9 @@ func handle_input():
 				if(can_climb):
 					current_state = State.STATE_CLIMB
 					hug_wall()
-				else:
-					current_state = State.STATE_GLIDE
+			
+			if(Input.is_action_just_pressed("jump")):
+				current_state = State.STATE_GLIDE
 		
 		State.STATE_GLIDE:
 			speed = Vector2(glide_speed.x * sign(speed.x), glide_speed.y)
@@ -111,34 +157,57 @@ func handle_input():
 			if(can_climb and Input.is_action_pressed("ui_up")):
 				current_state = State.STATE_CLIMB
 				hug_wall()
+			
+			if(not is_on_floor() and Input.is_action_pressed("ui_down")):
+				current_state = State.STATE_FALL
+		
+		State.STATE_ATTACK:
+			#speed = Vector2(0, 0)
+			current_state = state_after_attack
 
 func hug_wall():
-	if(current_building_to_climb_anchor_node.name == "Left"):
-		position.x = current_building_to_climb_anchor_node.global_position.x - $Sprite.texture.get_width()/2.0
+	#if(current_building_to_climb_anchor_node.name == "Left"):
+	#	position.x = current_building_to_climb_anchor_node.global_position.x - $Sprite.texture.get_width()/2.0
 	
-	if(current_building_to_climb_anchor_node.name == "Right"):
-		position.x = current_building_to_climb_anchor_node.global_position.x
+	#if(current_building_to_climb_anchor_node.name == "Right"):
+	#	position.x = current_building_to_climb_anchor_node.global_position.x
+	pass
+
+func attack(state):
+	state_after_attack = state
+	current_state = State.STATE_ATTACK
+	change_animation("attack")
+	attacking = true
+
+#class MyCustomBuildingsByDistanceSorter:
+#    static func sort(area_a, area_b):
+#		area_a.get_children()[area_shape]
+#        if area_a. < area_b[0]:
+#            return true
+#        return false
 
 func _on_Area2D_area_shape_entered(area_id, area, area_shape, self_shape):
 	#TO DO yield an array of close walls and pick the closest one
-	print(area.get_parent().name + " IN")
+	#print(area.get_parent().name + " IN")
 	if(area.get_parent().is_in_group("buildings")):
-		current_building_to_climb_anchor_node = area.get_children()[area_shape]
-		can_climb = true
+		nearest_buildings_array.push_back(area)
+		#current_building_to_climb_anchor_node = area.get_children()[area_shape]
+		#can_climb = true
 
 func _on_Area2D_area_shape_exited(area_id, area, area_shape, self_shape):
-	print(area.get_parent().name + " OUT")
+	#print(area.get_parent().name + " OUT")
 	if(area.get_parent().is_in_group("buildings")):
-		can_climb = false
+		nearest_buildings_array.remove(nearest_buildings_array.find(area))
+		#can_climb = false
 
 func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
-	print(body.get_parent().name + " IN")
+	#print(body.get_parent().name + " IN")
 	if(body.get_parent().is_in_group("ceilings")):
 		#can_climb = false
-		position = Vector2(position.x, body.global_position.y - $Sprite.texture.get_height()/2 - 16)
+		position = Vector2(global_position.x, body.global_position.y - $Sprite.texture.get_height()/2 - 16)
 
 func _on_Area2D_body_shape_exited(body_id, body, body_shape, area_shape):
-	print(body.get_parent().name + " OUT")
+	#print(body.get_parent().name + " OUT")
 	if(body.get_parent().is_in_group("ceilings")):
 		#can_climb = false
 		#speed.y = 0
